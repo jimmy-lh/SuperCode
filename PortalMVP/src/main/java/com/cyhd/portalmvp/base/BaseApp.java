8aa4bfb1de4860598cfea4e49eb390fb49821057
lihe <m18573003568@gmail.com>
2017-09-08 17:57:34 +0800

    不使用PortalMVP模块

--- PortalMVP/src/main/java/com/cyhd/portalmvp/base/BaseApp.java
+++ PortalMVP/src/main/java/com/cyhd/portalmvp/base/BaseApp.java
@@ -1,14 +1,7 @@
 package com.cyhd.portalmvp.base;
 
-import android.text.TextUtils;
-
-import com.cyhd.portalmvp.BuildConfig;
 import com.lh.commonclasses.base.CommonBaseApp;
-import com.lh.commonclasses.utils.LogUtil;
 
-import org.swl.feedback.FeedBack;
-
-import swl.lib.common.SwlDeviceInfo;
 
 public abstract class BaseApp extends CommonBaseApp {
     protected static String sn;
@@ -16,21 +9,5 @@ public abstract class BaseApp extends CommonBaseApp {
     @Override
     public void onCreate() {
         super.onCreate();
-    }
-
-    public String getSn() {
-        if (TextUtils.isEmpty(sn)) {
-            sn = SwlDeviceInfo.getStbSn();
-            if (TextUtils.isEmpty(sn)) {
-                sn = BuildConfig.DEFAULT_SN;
-            }
-            sn.trim();
-        }
-        LogUtil.d(TAG, "sn = " + sn);
-        return sn;
-    }
-
-    public void initFeedback() {
-        FeedBack.init(this);
     }
 }
