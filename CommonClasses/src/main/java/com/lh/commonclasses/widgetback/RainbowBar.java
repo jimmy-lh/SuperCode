e4db62f6003fc8745465449211baab8053ab30b3
lihe <m18573003568@gmail.com>
2017-09-28 15:17:40 +0800

    增加rainbowBar自定义View

---
+++ CommonClasses/src/main/java/com/lh/commonclasses/widgetback/RainbowBar.java
@@ -0,0 +1,87 @@
+package com.lh.commonclasses.widgetback;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.lh.commonclasses.R;
+import com.lh.commonclasses.utils.MetricsUtil;
+
+
+/**
+ * 彩虹bar
+ */
+public class RainbowBar extends View {
+
+    //progress bar color
+    int barColor = Color.parseColor("#1E88E5");
+    //every bar segment width
+    int hSpace = MetricsUtil.DipToPixels(getContext(), 80);
+    //every bar segment height
+    int vSpace = MetricsUtil.DipToPixels(getContext(), 4);
+    //space among bars
+    int space = MetricsUtil.DipToPixels(getContext(), 10);
+    float startX = 0;
+    float delta = 10f;
+    Paint mPaint;
+
+
+    public RainbowBar(Context context) {
+        this(context, null);
+    }
+
+    public RainbowBar(Context context, @Nullable AttributeSet attrs) {
+        this(context, null, 0);
+    }
+
+    public RainbowBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        //read custom attrs
+        TypedArray t = context.obtainStyledAttributes(attrs, R.styleable.rainbowbar, 0, 0);
+        hSpace = t.getDimensionPixelSize(R.styleable.rainbowbar_rainbowbar_hspace, hSpace);
+        vSpace = t.getDimensionPixelOffset(R.styleable.rainbowbar_rainbowbar_vspace, vSpace);
+        barColor = t.getColor(R.styleable.rainbowbar_rainbowbar_color, barColor);
+        t.recycle(); // we should always recycle after used
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(barColor);
+        mPaint.setStrokeWidth(vSpace);
+    }
+
+    //draw be invoke numbers.
+    int index = 0;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //get screen width
+        float sw = this.getMeasuredWidth();
+        if (startX >= sw + (hSpace + space) - (sw % (hSpace + space))) {
+            startX = 0;
+        } else {
+            startX += delta;
+        }
+        float start = startX;
+        // draw latter parse
+        while (start < sw) {
+            canvas.drawLine(start, 5, start + hSpace, 5, mPaint);
+            start += (hSpace + space);
+        }
+        start = startX - space - hSpace;
+        // draw front parse
+        while (start >= -hSpace) {
+            canvas.drawLine(start, 5, start + hSpace, 5, mPaint);
+            start -= (hSpace + space);
+        }
+        if (index >= 700000) {
+            index = 0;
+        }
+        invalidate();
+    }
+}
