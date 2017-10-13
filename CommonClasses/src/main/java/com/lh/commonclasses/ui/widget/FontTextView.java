5f97e414b13fa01848b872b0f349094dd1ca9fe8
lihe <m18573003568@gmail.com>
2017-10-13 14:57:27 +0800

    增加自定义字体的TextView

---
+++ CommonClasses/src/main/java/com/lh/commonclasses/ui/widget/FontTextView.java
@@ -0,0 +1,31 @@
+package com.lh.commonclasses.ui.widget;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * 加载自定义字体
+ */
+public class FontTextView extends TextView {
+    public FontTextView(Context context) {
+        this(context, null);
+    }
+
+    public FontTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FontTextView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        Typeface face = Typeface.createFromAsset(getContext().getAssets(), "fonts/ariblk.TTF");
+        getPaint().setFakeBoldText(true);
+        setTypeface(face);
+    }
+}
